b0253e7d24af92bfe24f4a94ebedb50b5122ecec
Karthik Ambali <karthikambali@gmail.com>
2023-02-02 14:36:33 +0530

    1st trial

---
+++ client/src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Item from "../../components/Item";
+import { Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useDispatch, useSelector } from "react-redux";
+import { setItems } from "../../state";
+
+const ShoppingList = () => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("all");
+  const items = useSelector((state) => state.cart.items);
+  const breakPoint = useMediaQuery("(min-width:600px)");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  async function getItems() {
+    const items = await fetch(
+      "http://localhost:3000/api/items?populate=image",
+      { method: "GET" }
+    );
+    const itemsJson = await items.json();
+    dispatch(setItems(itemsJson.data));
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const fruits = items.filter(
+    (item) => item.attributes.category === "fruits"
+  );
+  const juice = items.filter(
+    (item) => item.attributes.category === "juice"
+  );
+  const biscuits = items.filter(
+    (item) => item.attributes.category === "biscuits"
+  );
+
+  return (
+    <Box width="80%" margin="80px auto">
+      <Typography variant="h3" textAlign="center">
+        Our Featured <b>Products</b>
+      </Typography>
+      <Tabs
+        textColor="primary"
+        indicatorColor="primary"
+        value={value}
+        onChange={handleChange}
+        centered
+        TabIndicatorProps={{ sx: { display: breakPoint ? "block" : "none" } }}
+        sx={{
+          m: "25px",
+          "& .MuiTabs-flexContainer": {
+            flexWrap: "wrap",
+          },
+        }}
+      >
+        <Tab label="ALL" value="all" />
+        <Tab label="FRUITS" value="fruits" />
+        <Tab label="JUICE" value="biscuits" />
+        <Tab label="BISCUIT" value="juice" />
+      </Tabs>
+      <Box
+        margin="0 auto"
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, 300px)"
+        justifyContent="space-around"
+        rowGap="20px"
+        columnGap="1.33%"
+      >
+        {value === "all" &&
+          items.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+        {value === "fruits" &&
+          fruits.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+        {value === "biscuits" &&
+          biscuits.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+        {value === "juice" &&
+          juice.map((item) => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default ShoppingList;
